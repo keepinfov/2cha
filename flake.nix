84e20ec7fa91a8dbd2da15b05fb02c899bee3b72
Keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-12-08 15:40:47 +0300

    Fix nix flake for properly work (#22)
    * Setup Nix flake for nix run github:keepinfov/2cha
    
    - Add apps output for `nix run` support
    - Add flake-compat for legacy nix-shell/nix-build support
    - Create default.nix and update shell.nix to use flake-compat
    - Add overlay for use in other flakes
    - Add static musl build target via .#static
    - Improve metadata with mainProgram and homepage
    
    * Fix flake.lock to reflect nixpkgs follows directive
    
    * Add flake-compat to outputs function arguments

--- flake.nix
+++ flake.nix
@@ -4,66 +4,119 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, flake-compat, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        
+
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          targets = [ 
-            "x86_64-unknown-linux-musl" 
+          targets = [
+            "x86_64-unknown-linux-musl"
             "aarch64-unknown-linux-musl"
           ];
         };
+
+        # Common package attributes
+        commonAttrs = {
+          pname = "2cha";
+          version = "0.6.2";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          meta = with pkgs.lib; {
+            description = "High-performance VPN utility with IPv4/IPv6 support";
+            homepage = "https://github.com/keepinfov/2cha";
+            license = licenses.mit;
+            maintainers = [];
+            mainProgram = "2cha";
+            platforms = platforms.linux;
+          };
+        };
+
+        # Default package (dynamic linking)
+        twocha = pkgs.rustPlatform.buildRustPackage (commonAttrs // {
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+        });
+
+        # Static musl build
+        twochaStatic = pkgs.pkgsStatic.rustPlatform.buildRustPackage (commonAttrs // {
+          pname = "2cha-static";
+          nativeBuildInputs = with pkgs.pkgsStatic; [ pkg-config ];
+
+          CARGO_BUILD_TARGET = "${system}-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+
+          meta = commonAttrs.meta // {
+            description = "High-performance VPN utility (statically linked)";
+          };
+        });
+
       in
       {
+        # Packages
+        packages = {
+          default = twocha;
+          "2cha" = twocha;
+          static = twochaStatic;
+        };
+
+        # Apps for `nix run`
+        apps = {
+          default = {
+            type = "app";
+            program = "${twocha}/bin/2cha";
+          };
+          "2cha" = {
+            type = "app";
+            program = "${twocha}/bin/2cha";
+          };
+          static = {
+            type = "app";
+            program = "${twochaStatic}/bin/2cha";
+          };
+        };
+
+        # Development shell
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             rustToolchain
             rust-analyzer
+            cargo-watch
             gcc
             pkg-config
             musl
           ];
-          
+
           RUST_BACKTRACE = 1;
-          
+          RUST_LOG = "debug";
+
           shellHook = ''
-            echo "🔐 2cha development environment"
+            echo "2cha development environment"
             echo "Rust: $(rustc --version)"
+            echo ""
+            echo "Commands:"
+            echo "  cargo build --release              # Build release"
+            echo "  cargo run -- --help                # Run with args"
+            echo "  nix build                          # Build with Nix"
+            echo "  nix build .#static                 # Build static binary"
           '';
         };
-
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "2cha";
-          version = "0.6.2";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          
-          meta = with pkgs.lib; {
-            description = "High-performance VPN utility";
-            license = licenses.mit;
-          };
-        };
-        
-        # Static musl build
-        packages.static = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
-          pname = "2cha";
-          version = "0.6.2";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          
-          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-          
-          meta = with pkgs.lib; {
-            description = "High-performance VPN utility (static)";
-            license = licenses.mit;
-          };
-        };
       }
-    );
+    ) // {
+      # Overlay for use in other flakes
+      overlays.default = final: prev: {
+        twocha = self.packages.${prev.system}.default;
+      };
+    };
 }
