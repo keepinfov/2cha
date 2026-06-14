3e95a4ddd448b3191bb3e1ddeb81abab2e069834
keepinfov <218463407+keepinfov@users.noreply.github.com>
2026-06-14 23:45:08 +0300

    chore(release): bump version to 1.3.0
    New config-management CLI and declarative NixOS module warrant a minor
    version bump from 1.2.0.

--- nix/package.nix
+++ nix/package.nix
@@ -15,7 +15,7 @@
       # Common package attributes
       commonAttrs = {
         pname = "2cha";
-        version = "1.2.0";
+        version = "1.3.0";
         src = ../.;
         cargoLock.lockFile = ../Cargo.lock;
 
